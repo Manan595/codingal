5b959082342f37dd446fa0953b2682fb8c83c1c0
manan595 <manan.patel498@education.nsw.gov.au>
2024-11-19 18:27:10 +1100

    pass

---
+++ pass.py
@@ -0,0 +1,14 @@
+for x in range (20):
+    if x %20 ==0:
+     print("I")
+
+    elif x %19 ==0:
+       pass
+
+    elif x %5 ==0:
+       print("Like")
+    
+    elif x %3 == 0:
+       print("trains ")
+    else:
+       print(x)
