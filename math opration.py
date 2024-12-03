0c95c48309da6447dd7b0c7f1839047cea54cc7c
manan595 <manan.patel498@education.nsw.gov.au>
2024-12-03 18:40:35 +1100

    math oprations

---
+++ math opration.py
@@ -0,0 +1,10 @@
+import math 
+print('The Floor and celling value of 23.56 are :'+ str(math.ceil(23.56))+', '+str(math.floor(23.56)))
+
+x=10
+y=-15
+print('The value x after copying the sign fomr y is:'+str(math.copysign(x,y)))
+
+print('Absolute value of -96 and 56 are'+ str(math.fabs(-96))+','+ str(math.fabs(56)))
+
+print("the gcd of 24 and 56:"+str(math.gcd(24,56)))
