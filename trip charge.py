d1fdaccb08bc6bfbb31ea82801884a6b4e7d227d
manan595 <manan.patel498@education.nsw.gov.au>
2024-12-10 19:20:40 +1100

    turi

--- trip charge.py
+++ trip charge.py
@@ -1,7 +1,7 @@
 def hotel_cost(nights):
     return 140*nights
 
-def planet_ride_cost(city):
+def plane_ride_cost(city):
     if "charlotte" == city:
      return 183
 
@@ -12,3 +12,22 @@ def planet_ride_cost(city):
     elif' Los angles' == city:
        return 447
     
+    def rental_car_cost(days):
+       if days>=7:
+          return 40*days -50
+       elif days>=3 :
+          return 40*days -20
+       else:
+          return 40*days
+def trip_cost(city, days, spending_money):
+   return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
+
+print("cost of rental car",rental_car_cost(5))
+
+print("cost of a planet ride", plane_ride_cost("Los angles "))
+
+print("cost of a hotel room")
+
+print("The total cost of the trip", trip_cost("Los angles",7,500))
+
+print(trip_cost("Tampa",6,500))
