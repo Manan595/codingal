7625a5e4b427665d68737fbb83594dccbb6fc673
manan595 <manan.patel498@education.nsw.gov.au>
2024-12-10 18:42:56 +1100

    trip charge

---
+++ trip charge.py
@@ -0,0 +1,14 @@
+def hotel_cost(nights):
+    return 140*nights
+
+def planet_ride_cost(city):
+    if "charlotte" == city:
+     return 183
+
+    elif 'Tampa'== city :
+       return 220
+    elif 'pittsburgh' == city:
+       return 222
+    elif' Los angles' == city:
+       return 447
+    
