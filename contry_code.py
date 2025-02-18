0855a4f6af4875df3a2028b8c9e31592f1131924
manan595 <manan.patel498@education.nsw.gov.au>
2025-02-18 18:28:48 +1100

    contry

---
+++ contry_code.py
@@ -0,0 +1,8 @@
+contry_code={'India': '0091',
+             'australia':'0025',
+             'nepal':'00977'}
+print('contry code for India -')
+print(contry_code.get('India','Not found'))
+
+print('contry code japan')
+print(contry_code.get ('japan','not found '))
