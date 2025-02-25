f29fd78c46d14c4b1d02a49743230e1529f4197c
manan595 <manan.patel498@education.nsw.gov.au>
2025-02-25 18:34:25 +1100

    set intersection

---
+++ set_intersection.py
@@ -0,0 +1,10 @@
+setx={'green','blue'}
+sety={'blue','yellow'}
+print('Orginal set elements:')
+print(sety)
+print(setx)
+print('intersecsion of two said sets:')
+setz= setx.intersection(sety)
+print(setz)
+setz1=setx.union(sety) 
+print(setz1)
