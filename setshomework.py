c548ef97708c7b2442db1350103b3892e73c13e5
manan595 <manan.patel498@education.nsw.gov.au>
2025-02-25 19:09:12 +1100

    set

---
+++ setshomework.py
@@ -0,0 +1,11 @@
+setz2=setx.difference(sety)
+print(setz2)
+
+setz3=sety.difference(setx)
+print(setz3)
+
+setz4=setx.symmetric_difference(sety)
+print(setz4)
+
+setz5= sety.symmetric_difference(setx)
+print(setz5)
