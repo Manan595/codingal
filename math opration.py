41a92437fcfe3fe9fa2218b24c6e961a9cfcbc3c
manan595 <manan.patel498@education.nsw.gov.au>
2025-02-25 18:25:45 +1100

    set

--- math opration.py
+++ math opration.py
@@ -1,10 +1,10 @@
 import math 
-print('The Floor and celling value of 23.56 are :'+ str(math.ceil(23.56))+', '+str(math.floor(23.56)))
+print('The Floor and celling value of 23.56 are :'+ str(math.ceil(7))+', '+str(math.floor(7)))
 
 x=10
 y=-15
 print('The value x after copying the sign fomr y is:'+str(math.copysign(x,y)))
 
-print('Absolute value of -96 and 56 are'+ str(math.fabs(-96))+','+ str(math.fabs(56)))
+print('Absolute value of -96 and 56 are'+ str(math.fabs(7))+','+ str(math.fabs(7)))
 
-print("the gcd of 24 and 56:"+str(math.gcd(24,56)))
+print("the gcd of 24 and 56:"+str(math.gcd(49,7)))
