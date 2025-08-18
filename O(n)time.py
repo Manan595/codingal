0ef54efaf0312b20983622857a0af841b48e0fe8
manan595 <manan.patel498@education.nsw.gov.au>
2025-08-18 17:43:42 +1000

    o(n)time

---
+++ O(n)time.py
@@ -0,0 +1,10 @@
+def ontime(n):
+    iterations=0
+    for i in range(1,n+1):
+        iterations+=1
+    print("when n is ",n,"iteratoins=iterations")
+ontime(10)
+ontime(20)
+ontime(42)
+
+print("\nWith every 'n' the time taken in iterations will incrase")
