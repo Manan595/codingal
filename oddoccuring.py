c8bd8aeb3f5e336d3426b899d84971fc76204411
manan595 <manan.patel498@education.nsw.gov.au>
2025-10-13 18:36:09 +1100

    odd occuring

---
+++ oddoccuring.py
@@ -0,0 +1,15 @@
+def oddoccuringnumber(arr):
+    res=0
+    for element in arr:
+        res=res ^ element
+
+    return res
+
+arr=[]
+n=int(input('enter array size:'))
+while(n):
+    num=int(input('enter number:'))
+    arr.append(num)
+    n-=1
+
+print('odd occuring number is:', oddoccuringnumber(arr))
