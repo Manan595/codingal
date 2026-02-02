2b25d155eef8dc9c028b971a8104a50ad1fac085
manan595 <manan.patel498@education.nsw.gov.au>
2026-02-02 18:44:05 +1100

    roations

---
+++ roation.py
@@ -0,0 +1,23 @@
+def rotation(a,n,a_size):
+    for i in range(n):
+        rotate(a,a_size)
+
+def rotate(a,a_size):
+    temp=a[0]
+    for i in  range(a_size)-1:
+          a[i]=a[i+1]
+          a[a,a_size -1]=temp
+
+def printarray(a,a_size-1)
+a[i]=a[i+1]
+a[a_size-1]=temp
+
+a[12,12,31,80,2,5,53,56323]
+print array (a,len(a))
+rotations(a,2, len(a))
+printarray (a,len(a))
+
+
+
+        
+        
